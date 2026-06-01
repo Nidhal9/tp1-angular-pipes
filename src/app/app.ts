e38e4b97d15ae102ac214f40123706d261b42ba9
nidhal <nidhalinoubliz9@gmail.com>
2026-06-01 23:28:05 +0100

    TP1 Angular - Pipes terminé

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResumePipe } from './resume.pipe';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  standalone: false,
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
+  standalone: true,  // ← important
+  imports: [         // ← importer tous les pipes ici
+    CommonModule,    // contient uppercase, lowercase, currency, percent, etc.
+    ResumePipe       // votre pipe personnalisé
+  ]
 })
 export class App {
-  protected readonly title = signal('tp1-pipes');
+  name = 'bilel amdouni';
+  price = 1234.5;
+  today = new Date();
+  percentage = 0.75;
+  message = 'angular est puissant';
+  users = ['Ali', 'Sarra', 'Mohamed'];
 }
---
+++ src/app/resume.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'resume'
+})
+export class ResumePipe implements PipeTransform {
+
+  transform(value: string, limit: number = 10): string {
+    if (!value) return '';
+    return value.length > limit ? value.substring(0, limit) + '...' : value;
+  }
+
+}
